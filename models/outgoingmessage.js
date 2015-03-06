963df3eba0431f516768cfbae63570559a996e5f
Dex Fitch <fitch.dexter@gmail.com>
2015-03-06 12:04:02 -0800

    Add routes, template, and controllers for outgoing messages.

--- models/outgoingmessage.js
+++ models/outgoingmessage.js
@@ -1,4 +1,7 @@
 Junkmail.Outgoingmessage = DS.Model.extend({
+  email: DS.attr(),
+  name: DS.attr(),
   title: DS.attr(),
+  date: DS.attr(),
   body: DS.attr()
 })
--- router.js
+++ router.js
@@ -2,5 +2,7 @@ Junkmail.Router.map(function() {
   this.resource('inboxmessages', {path: '/'}, function() {
     this.resource('welcome');
   });
-  this.resource('outgoingmessages');
+  this.resource('outgoingmessages', function() {
+    this.resource('outgoingmessage', {path: 'outgoingmessages/:outgoingmessage_id'});
+  });
 });
