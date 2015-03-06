670c154e218f89d5dab458326f5ae65471656de2
Dex Fitch <fitch.dexter@gmail.com>
2015-03-06 13:03:32 -0800

    Some refactoring, still trying to track down bug with rendering 'sent' messages.

--- router.js
+++ router.js
@@ -1,9 +1,18 @@
 Junkmail.Router.map(function() {
-  this.resource('inboxmessages', {path: '/'}, function() {
-    this.resource('welcome');
-  });
-  this.resource('outgoingmessages', function() {
-    this.resource('outgoingmessage', {path: 'outgoingmessages/:outgoingmessage_id'});
-  });
-  this.resource('compose');
+  this.resource('inboxmessages', {path: '/'});
+  this.resource('welcome');
+  this.resource('outgoingmessages');
+  this.resource('outgoingmessage', {path: 'outgoingmessages/:outgoingmessage_id'});
+  this.resource('new-outgoingmessage');
 });
+
+
+// Junkmail.Router.map(function() {
+//   this.resource('inboxmessages', {path: '/'}, function() {
+//     this.resource('welcome');
+//   });
+//   this.resource('outgoingmessages', function() {
+//     this.resource('outgoingmessage', {path: 'outgoingmessages/:outgoingmessage_id'});
+//   });
+//   this.resource('new-outgoingmessage');
+// });
